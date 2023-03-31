640b04635ebca3c93d2862fed62d0ace8c177d16
koeda <yohannesteshle@gmail.com>
2023-03-31 12:22:17 -0500

    added yml config file

--- src/main/java/com/optimal/license/optimallicense_configuration_server/OptimalLicenseConfigurationServerApplication.java
+++ src/main/java/com/optimal/license/optimallicense_configuration_server/OptimalLicenseConfigurationServerApplication.java
@@ -2,8 +2,10 @@ package com.optimal.license.optimallicense_configuration_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class OptimalLicenseConfigurationServerApplication {
 
     public static void main(String[] args) {
