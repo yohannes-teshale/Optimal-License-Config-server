2b3ac454b5a3f4ab90a70d19f5c3a67151f80e14
koeda <yohannesteshle@gmail.com>
2023-03-31 11:11:46 -0500

    first commit

---
+++ src/main/java/com/optimal/license/optimallicense_configuration_server/OptimalLicenseConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.optimal.license.optimallicense_configuration_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalLicenseConfigurationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimalLicenseConfigurationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optimal/license/optimallicense_configuration_server/OptimalLicenseConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimal.license.optimallicense_configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalLicenseConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
